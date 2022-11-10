4fdfc0e829d5b784156d32a4ede7b463a7864365
MatveevYura <matvssv.yura@gmail.com>
2022-11-11 01:43:57 +0200

    page 3 done

--- dist/js/app.js
+++ dist/js/app.js
@@ -34,7 +34,7 @@
     var secondOutput = document.getElementById("Description");
     secondOutput.innerHTML = `<label>Modest</label><span class="Value"><span class="Percent">20-60%</span> of sky</span>`;
     secondSlider.oninput = function() {
-        if (0 == secondSlider.value) secondOutput.innerHTML = `<label>None or very little</label><span class="Value">less than <span class="Percent">20%</span> of sky</span>`; else if (1 == secondSlider.value) secondOutput.innerHTML = `<label>Modest</label><span class="Value"><span class="Percent">20-60%</span> of sky</span>`; else if (2 == secondSlider.value) secondOutput.innerHTML = `<label>Significant</label><span class="Value"><span class="Percent">60-80%</span> of sky</span>`; else secondOutput.innerHTML = `<label>Heavy</label><span class="Value">more than<span class="Percent">80%</span>of sky</span>`;
+        if (0 == secondSlider.value) secondOutput.innerHTML = `<label>None or very little</label><span class="Value">less than <span class="Percent">20%</span> of sky</span>`; else if (1 == secondSlider.value) secondOutput.innerHTML = `<label>Modest</label><span class="Value"><span class="Percent">20-60%</span> of sky</span>`; else if (2 == secondSlider.value) secondOutput.innerHTML = `<label>Significant</label><span class="Value"><span class="Percent">60-80%</span> of sky</span>`; else if (3 == secondSlider.value) secondOutput.innerHTML = `<label>Heavy</label><span class="Value">more than<span class="Percent">80%</span>of sky</span>`;
         var pxBackgroundPosition = parseInt(112 * secondSlider.value);
         $(".Diagram").css("background-position", "0px " + (0 - pxBackgroundPosition) + "px");
     };
@@ -55,9 +55,17 @@
     var step2BackButton = document.getElementById("step2BackButton");
     step2BackButton.addEventListener("click", step2BackButtonHandler);
     function step2BackButtonHandler() {
-        document.getElementById("rmpView").classList.toggle("hidden");
-        document.getElementById("card").classList.toggle("hidden");
+        document.getElementById("rmpView").classList.add("hidden");
+        document.getElementById("card").classList.remove("hidden");
     }
+    var step3BackButton = document.getElementById("step3BackButton");
+    step3BackButton.addEventListener("click", step3BackButtonHandler);
+    function step3BackButtonHandler() {
+        document.getElementById("rmpView").classList.remove("hidden");
+        document.getElementById("resultsBlock").classList.add("hidden");
+        document.getElementById("header__block").classList.remove("hidden");
+    }
+    step3BackButton;
     function DrawMap(getNum) {
         var geocoder = new google.maps.Geocoder;
         var address = getNum;
@@ -78,8 +86,8 @@
                 };
                 var infoWindow = new google.maps.InfoWindow;
                 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                document.getElementById("card").classList.toggle("hidden");
-                document.getElementById("rmpView").classList.toggle("hidden");
+                document.getElementById("card").classList.add("hidden");
+                document.getElementById("rmpView").classList.remove("hidden");
                 document.getElementById("mapAllert").classList.add("hidden");
                 var marker;
                 (function(marker, data) {
@@ -92,11 +100,81 @@
             } else document.getElementById("mapAllert").classList.remove("hidden"); else document.getElementById("mapAllert").classList.remove("hidden");
         }));
     }
+    document.getElementById("resultsBlock").classList.add("hidden");
     var fourSlider = document.getElementById("fourSlider");
     fourSlider.oninput = function() {
         var pxBackgroundPosition = parseInt(100 * fourSlider.value);
         $(".Diagram").css("background-position", "0px " + (0 - pxBackgroundPosition) + "px");
     };
+    var step2NextButton = document.getElementById("step2NextButton");
+    step2NextButton.addEventListener("click", step2NextButtonHandler);
+    function step2NextButtonHandler() {
+        calculations();
+        document.getElementById("rmpView").classList.add("hidden");
+        document.getElementById("header__block").classList.add("hidden");
+        document.getElementById("card").classList.add("hidden");
+        document.getElementById("resultsBlock").classList.remove("hidden");
+    }
+    function getRandomArbitrary(min = 250, max = 280) {
+        return Math.random() * (max - min) + min;
+    }
+    function getRandomCO2(min = 640, max = 650) {
+        return Math.random() * (max - min) + min;
+    }
+    function getRandomFuel(min = 180, max = 190) {
+        return Math.random() * (max - min) + min;
+    }
+    function getRandomSEG(min = 120, max = 130) {
+        return Math.random() * (max - min) + min;
+    }
+    var getRandomTest = 10;
+    function getRandomGenerate(min = 1350, max = 1360) {
+        return Math.random() * (max - min) + min;
+    }
+    function calculations() {
+        var sizeOfTheSystem = document.getElementById("sizeOfTheSystem");
+        var intGenerationID = document.getElementById("intGenerationID");
+        if (0 != intGenerationID.value) {
+            sizeOfTheSystem.innerHTML = intGenerationID.value;
+            if (2 != intGenerationID.value) var panel = (intGenerationID.value - 2) / .5 * 493 + 3971; else panel = 3971;
+        } else if (0 == fourSlider.value) {
+            sizeOfTheSystem.innerHTML = 2;
+            panel = 3971;
+        } else if (1 == fourSlider.value) {
+            sizeOfTheSystem.innerHTML = 4;
+            panel = (sizeOfTheSystem.innerHTML - 2) / .5 * 493 + 3971;
+        } else if (2 == fourSlider.value) {
+            sizeOfTheSystem.innerHTML = 6;
+            panel = (sizeOfTheSystem.innerHTML - 2) / .5 * 493 + 3971;
+        } else if (3 == fourSlider.value) {
+            sizeOfTheSystem.innerHTML = 8;
+            panel = (sizeOfTheSystem.innerHTML - 2) / .5 * 493 + 3971;
+        }
+        if (FirstSlider.value <= 15 || FirstSlider.value >= 55) var newFirstSlider = .9; else newFirstSlider = 1;
+        if (0 == secondSlider.value) var newSecondSlider = 1; else if (1 == secondSlider.value) newSecondSlider = .8; else if (2 == secondSlider.value) newSecondSlider = .6; else if (3 == secondSlider.value) newSecondSlider = .4;
+        var potentialAnnualBenefit = document.getElementById("potentialAnnualBenefit");
+        var potentialCO2saving = document.getElementById("potentialCO2saving");
+        var potentialFuelBillSaving = document.getElementById("potentialFuelBillSaving");
+        var potentialPaymentsFromSEG = document.getElementById("potentialPaymentsFromSEG");
+        var potentialLifetimeBenefit = document.getElementById("potentialLifetimeBenefit");
+        var potentialLifetimeCO2Saving = document.getElementById("potentialLifetimeCO2Saving");
+        var potentialLifetimeFuelBillSaving = document.getElementById("potentialLifetimeFuelBillSaving");
+        var potentialLifetimePaymentsFromSEG = document.getElementById("potentialLifetimePaymentsFromSEG");
+        var estimatedInstallationCosts = document.getElementById("estimatedInstallationCosts");
+        var potentialLifetimeNetBenefit = document.getElementById("potentialLifetimeNetBenefit");
+        var energyGeneratedByThePanels = document.getElementById("energyGeneratedByThePanels");
+        potentialAnnualBenefit.innerHTML = getRandomArbitrary().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (.5 * sizeOfTheSystem.innerHTML).toFixed(0);
+        potentialCO2saving.innerHTML = getRandomCO2().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (.5 * sizeOfTheSystem.innerHTML).toFixed(0);
+        potentialFuelBillSaving.innerHTML = getRandomFuel().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (.5 * sizeOfTheSystem.innerHTML).toFixed(0);
+        potentialPaymentsFromSEG.innerHTML = getRandomSEG().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (.5 * sizeOfTheSystem.innerHTML).toFixed(0);
+        potentialLifetimeBenefit.innerHTML = 24 * potentialAnnualBenefit.innerHTML;
+        potentialLifetimeCO2Saving.innerHTML = 24 * potentialCO2saving.innerHTML;
+        potentialLifetimeFuelBillSaving.innerHTML = 24 * potentialFuelBillSaving.innerHTML;
+        potentialLifetimePaymentsFromSEG.innerHTML = 24 * potentialPaymentsFromSEG.innerHTML;
+        estimatedInstallationCosts.innerHTML = panel.toFixed(0);
+        potentialLifetimeNetBenefit.innerHTML = getRandomTest.toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (.5 * sizeOfTheSystem.innerHTML).toFixed(0);
+        energyGeneratedByThePanels.innerHTML = getRandomGenerate().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (.5 * sizeOfTheSystem.innerHTML).toFixed(0);
+    }
     window["FLS"] = true;
     isWebp();
 })();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -33,7 +33,7 @@ secondSlider.oninput = function () {
     secondOutput.innerHTML = `<label>Modest</label><span class="Value"><span class="Percent">20-60%</span> of sky</span>`;
   } else if (secondSlider.value == 2) {
     secondOutput.innerHTML = `<label>Significant</label><span class="Value"><span class="Percent">60-80%</span> of sky</span>`;
-  } else {
+  } else if (secondSlider.value == 3) {
     secondOutput.innerHTML = `<label>Heavy</label><span class="Value">more than<span class="Percent">80%</span>of sky</span>`;
   }
   var pxBackgroundPosition = parseInt(secondSlider.value * 112);
@@ -77,10 +77,20 @@ var step2BackButton = document.getElementById('step2BackButton');
 step2BackButton.addEventListener('click', step2BackButtonHandler);
 
 function step2BackButtonHandler() {
-  document.getElementById('rmpView').classList.toggle('hidden');
-  document.getElementById('card').classList.toggle('hidden');
+  document.getElementById('rmpView').classList.add('hidden');
+  document.getElementById('card').classList.remove('hidden');
 }
+var step3BackButton = document.getElementById('step3BackButton');
 
+step3BackButton.addEventListener('click', step3BackButtonHandler);
+
+function step3BackButtonHandler() {
+  document.getElementById('rmpView').classList.remove('hidden');
+  document.getElementById('resultsBlock').classList.add('hidden');
+  document.getElementById('header__block').classList.remove('hidden');
+
+}
+step3BackButton
 
 //========================================================================================================================================================
 // MAP Create
@@ -108,8 +118,8 @@ function DrawMap(getNum) {
 
         //========================================================================================================================================================
         // NEED DELETE COMMENT
-        document.getElementById('card').classList.toggle('hidden');
-        document.getElementById('rmpView').classList.toggle('hidden');
+        document.getElementById('card').classList.add('hidden');
+        document.getElementById('rmpView').classList.remove('hidden');
         document.getElementById("mapAllert").classList.add('hidden');
 
         //========================================================================================================================================================
@@ -133,6 +143,7 @@ function DrawMap(getNum) {
 
 //========================================================================================================================================================
 
+document.getElementById('resultsBlock').classList.add('hidden');
 
 var fourSlider = document.getElementById("fourSlider");
 //var mapOutput = document.getElementById("hiddenValueMap");
@@ -146,4 +157,121 @@ fourSlider.oninput = function () {
 
   var pxBackgroundPosition = parseInt(fourSlider.value * 100);
   $('.Diagram').css('background-position', '0px ' + (0 - (pxBackgroundPosition)) + 'px');
+}
+
+var step2NextButton = document.getElementById('step2NextButton');
+
+step2NextButton.addEventListener('click', step2NextButtonHandler);
+
+function step2NextButtonHandler() {
+  calculations()
+  document.getElementById('rmpView').classList.add('hidden');
+  document.getElementById('header__block').classList.add('hidden');
+  document.getElementById('card').classList.add('hidden');
+  document.getElementById('resultsBlock').classList.remove('hidden');
+
+}
+//========================================================================================================================================================
+/* function getRandomArbitrary(min = 230, max = 280) {
+  return Math.random() * (max - min) + min;
+} */
+
+function getRandomArbitrary(min = 250, max = 280) {
+  return Math.random() * (max - min) + min;
+}
+function getRandomCO2(min = 640, max = 650) {
+  return Math.random() * (max - min) + min;
+}
+function getRandomFuel(min = 180, max = 190) {
+  return Math.random() * (max - min) + min;
+}
+function getRandomSEG(min = 120, max = 130) {
+  return Math.random() * (max - min) + min;
+}
+var getRandomTest = 10
+function getRandomGenerate(min = 1350, max = 1360) {
+  return Math.random() * (max - min) + min;
+}
+
+function calculations() {
+  //========================================================================================================================================================
+  //SELECT
+  var sizeOfTheSystem = document.getElementById('sizeOfTheSystem');
+  var intGenerationID = document.getElementById('intGenerationID');
+  if (intGenerationID.value != 0) {
+    sizeOfTheSystem.innerHTML = intGenerationID.value
+    if (intGenerationID.value != 2) {
+      var panel = (intGenerationID.value - 2) / 0.5 * 493 + 3971
+    } else {
+      panel = 3971
+    }
+  } else if (fourSlider.value == 0) {
+    sizeOfTheSystem.innerHTML = 2
+    panel = 3971
+  } else if (fourSlider.value == 1) {
+    sizeOfTheSystem.innerHTML = 4
+    panel = (((sizeOfTheSystem.innerHTML - 2) / 0.5) * 493) + 3971
+  } else if (fourSlider.value == 2) {
+    sizeOfTheSystem.innerHTML = 6
+    panel = (((sizeOfTheSystem.innerHTML - 2) / 0.5) * 493) + 3971
+  } else if (fourSlider.value == 3) {
+    sizeOfTheSystem.innerHTML = 8
+    panel = (((sizeOfTheSystem.innerHTML - 2) / 0.5) * 493) + 3971
+  }
+  //========================================================================================================================================================
+  //FirstSlider
+  if ((FirstSlider.value <= 15) || (FirstSlider.value >= 55)) {
+    var newFirstSlider = 0.9
+  } else {
+    newFirstSlider = 1
+  }
+  //========================================================================================================================================================
+  if (secondSlider.value == 0) {
+    var newSecondSlider = 1
+  } else if (secondSlider.value == 1) {
+    var newSecondSlider = 0.8
+  } else if (secondSlider.value == 2) {
+    var newSecondSlider = 0.6
+  } else if (secondSlider.value == 3) {
+    var newSecondSlider = 0.4
+  }
+
+  var potentialAnnualBenefit = document.getElementById('potentialAnnualBenefit')
+  var potentialCO2saving = document.getElementById('potentialCO2saving')
+  var potentialFuelBillSaving = document.getElementById('potentialFuelBillSaving')
+  var potentialPaymentsFromSEG = document.getElementById('potentialPaymentsFromSEG')
+
+  var potentialLifetimeBenefit = document.getElementById('potentialLifetimeBenefit')
+  var potentialLifetimeCO2Saving = document.getElementById('potentialLifetimeCO2Saving')
+  var potentialLifetimeFuelBillSaving = document.getElementById('potentialLifetimeFuelBillSaving')
+  var potentialLifetimePaymentsFromSEG = document.getElementById('potentialLifetimePaymentsFromSEG')
+
+  var estimatedInstallationCosts = document.getElementById('estimatedInstallationCosts')
+  var potentialLifetimeNetBenefit = document.getElementById('potentialLifetimeNetBenefit')
+  var energyGeneratedByThePanels = document.getElementById('energyGeneratedByThePanels')
+
+
+  potentialAnnualBenefit.innerHTML = getRandomArbitrary().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (sizeOfTheSystem.innerHTML * 0.5).toFixed(0);
+  potentialCO2saving.innerHTML = getRandomCO2().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (sizeOfTheSystem.innerHTML * 0.5).toFixed(0);
+  potentialFuelBillSaving.innerHTML = getRandomFuel().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (sizeOfTheSystem.innerHTML * 0.5).toFixed(0);
+  potentialPaymentsFromSEG.innerHTML = getRandomSEG().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (sizeOfTheSystem.innerHTML * 0.5).toFixed(0);
+
+  potentialLifetimeBenefit.innerHTML = potentialAnnualBenefit.innerHTML * 24
+  potentialLifetimeCO2Saving.innerHTML = potentialCO2saving.innerHTML * 24
+  potentialLifetimeFuelBillSaving.innerHTML = potentialFuelBillSaving.innerHTML * 24
+  potentialLifetimePaymentsFromSEG.innerHTML = potentialPaymentsFromSEG.innerHTML * 24
+
+  estimatedInstallationCosts.innerHTML = panel.toFixed(0)
+  potentialLifetimeNetBenefit.innerHTML = getRandomTest.toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (sizeOfTheSystem.innerHTML * 0.5).toFixed(0);
+  energyGeneratedByThePanels.innerHTML = getRandomGenerate().toFixed(0) * newFirstSlider.toFixed(0) * newSecondSlider.toFixed(0) * (sizeOfTheSystem.innerHTML * 0.5).toFixed(0);
+
+
+
+
+
+
+
+
+
+
 }
